5ace17d3f355e90eada435c10aa6689d7c535c6a
Sam21sop <sbussiness21@gmail.com>
2024-01-27 17:12:30 +0530

    replace text implement

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -1,37 +1,58 @@
-module.exports = function(grunt) {
-
-    // intialize configuration object
-    grunt.initConfig({
-
-        // uglify javascript file
-        uglify:{
-            my_target:{
-                files: {
-                    'dest/min.js': ['src/input1.js']
-                }
-            }
+module.exports = function (grunt) {
+  // intialize configuration object
+  grunt.initConfig({
+    // uglify javascript file
+    uglify: {
+      my_target: {
+        files: {
+          "dest/min.js": ["src/input1.js"],
         },
+      },
+    },
 
-        // configure minify css
-        cssmin:{
-            target:{
-                files:[
-                    {
-                        expand: true,
-                        cwd: "src", 
-                        src: ["*.css", "!*min.css"],
-                        dest: "dest",
-                        ext: ".min.css"
-                    }
-                ]
-            }
-        }
-    });
+    // configure minify css
+    cssmin: {
+      target: {
+        files: [
+          {
+            expand: true,
+            cwd: "src",
+            src: ["*.css", "!*min.css"],
+            dest: "dest",
+            ext: ".min.css",
+          },
+        ],
+      },
+    },
 
-    // load libraries
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // Text Replacement
+    replace: {
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: /ninjacoding\.com/g,
+              replacement: "codingninjas.com",
+            },
+          ],
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ["src/index.html"],
+            dest: "build/",
+          },
+        ],
+      },
+    },
+  });
 
-    //setting up task
-    grunt.registerTask('default', ['uglify', 'cssmin']);
+  // load libraries
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-replace");
+
+  //setting up task
+  grunt.registerTask("default", ["uglify", "cssmin", "replace"]);
 };
